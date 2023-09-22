75f4531403cd931fc2faa17579188abf4dcc046d
JRhin <jrhin@proton.me>
2023-09-22 11:31:06 +0200

    update to the download script

--- src/download.py
+++ src/download.py
@@ -3,18 +3,22 @@ import zipfile
 from dotenv import dotenv_values
 from pathlib import Path
 
-current = Path('.')
-env_path = current/'.env'
-data_path = current/'data'
-zip_path = data_path/'data.zip'
+def main():
+    current = Path('.')
+    env_path = current/'.env'
+    data_path = current/'data'
+    zip_path = data_path/'data.zip'
 
-data_path.mkdir(exist_ok=True)
+    data_path.mkdir(exist_ok=True)
 
-env = dotenv_values(env_path)
+    env = dotenv_values(env_path)
 
-gdown.download(id=env['ID'], output=str(zip_path))
+    gdown.download(id=env['ID'], output=str(zip_path))
 
-with zipfile.ZipFile(str(zip_path),"r") as zip_ref:
-    zip_ref.extractall(data_path)
+    with zipfile.ZipFile(str(zip_path),"r") as zip_ref:
+        zip_ref.extractall(data_path)
 
 zip_path.unlink()
+
+if __name__ == "__main__":
+    main()
