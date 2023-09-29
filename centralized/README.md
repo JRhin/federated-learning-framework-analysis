c9ed80d48a82eef478b47844953ce8c8cf52e3c0
JRhin <jrhin@proton.me>
2023-09-29 14:04:12 +0200

    modified the run.sh script

---
+++ centralized/README.md
@@ -0,0 +1,11 @@
+# Centralized
+
+## How to run
+
+Is high recommended to firstly create a virtual enviroment.
+
+Then to run, just:
+
+```bash
+.\run.sh
+```
--- centralized/run.sh
+++ centralized/run.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+echo -e 'Installing needed python requirements...'
+pip install -q -r requirements.txt
+
+echo -e '\n\nDownload and partitioning the data...'
 python ./src/download.py
+
+echo -e '\n\nInitializing a Swarm'
 docker swarm init
+
+echo -e '\n\nDeploying a stack of containers'
 docker stack deploy -c docker-compose.yml my_stack
+
+echo -e '\nDONE'
