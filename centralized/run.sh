5c904bff4e7b8ab0d87793377312c2044a4021bc
JRhin <jrhin@proton.me>
2023-10-01 11:00:04 +0200

    now script creates the images

--- centralized/run.sh
+++ centralized/run.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo -e 'Installing needed python requirements...'
 pip install -q -r requirements.txt
 
 echo -e '\n\nDownload and partitioning the data...'
 python ./src/download.py
+
+echo -e "\n\nCreating the master-node image..."
+docker build -t master-node ./master
+
+echo -e "\n\nCreating the client-node image..."
+docker build -t client-node ./client
 
 echo -e '\n\nInitializing a Swarm'
 docker swarm init
